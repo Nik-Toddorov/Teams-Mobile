be02714147e5a1dc6e35f85fc1ebc7461eb7140d
Nik-Toddorov <nik.todorov1@gmail.com>
2026-09-17 00:08:06 +0300

    Add WebKit dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,4 +33,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.webkit:webkit:1.11.0")
 }
